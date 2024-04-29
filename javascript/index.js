bd48f842c2f86faa5ee3bdf0131e8498cce4ac2d
sandraluong <elieluong1993@gmail.com>
2024-04-29 22:04:20 +0200

    Add local time in select

--- javascript/index.js
+++ javascript/index.js
@@ -26,6 +26,9 @@ setInterval(updateTime, 1000);
 
 function showCityTime(event) {
   let cityTimeZone = event.target.value;
+  if (cityTimeZone === "current") {
+    cityTimeZone = moment.tz.guess();
+  }
   let cityName = cityTimeZone.replace("_", " ").split("/")[1];
   let cityTime = moment().tz(cityTimeZone);
   let citiesEl = document.querySelector("#cities");
